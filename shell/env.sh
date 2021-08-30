6afdd9177851ecd1109ae011bf5c65f995faa5b8
Kevin Murani <kevin.murani@booking.com>
2021-08-30 09:26:03 +0200

    Adds facility for machine specific env

--- shell/env.sh
+++ shell/env.sh
@@ -3,6 +3,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 fpath=($fpath "/Users/kmurani/.zfunctions")
 
-export perlcritic="/usr/local/Cellar/perl/5.28.1/bin/perlcritic"
-
-source ~/perl5/perlbrew/etc/bashrc
+# this is for machine specific settings that will not be tracked in git
+if [ -f $HOME/.env.machine.sh ]; then source $HOME/.env.machine.sh; fi
