593a90dfd630b79121b3d9a9e8cee7e55145382b
Kevin Murani <kevin.murani@booking.com>
2023-05-28 18:40:01 +0200

    Fixes spacehip opening as propt on session start

--- zsh/spaceship.zsh
+++ zsh/spaceship.zsh
@@ -1,14 +1,9 @@
-# Set Spaceship ZSH as a prompt
-autoload -U promptinit; promptinit
-prompt spaceship
+source "$HOME/.oh-my-zsh/custom/themes/spaceship.zsh-theme"
 
 ZSH_THEME="spaceship"
-
-source "$HOME/.oh-my-zsh/custom/themes/spaceship.zsh-theme"
 
 SPACESHIP_BATTERY_SHOW=true
 SPACESHIP_TIME_SHOW=true
 SPACESHIP_TIME_12HR=true
 SPACESHIP_GIT_STATUS_SHOW=false
-SPACESHIP_KUBECONTEXT_SHOW=false
-
+SPACESHIP_KUBECTL_CONTEXT_SHOW=false
