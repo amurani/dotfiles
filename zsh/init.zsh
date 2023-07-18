d73e5faa57b096bb4eefdc7cca5951820fecb12a
Kevin Murani <kevin.murani@booking.com>
2023-07-18 21:25:50 +0200

    Adds zsh theme to match others: catpuccin-mocha

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -4,5 +4,7 @@ source $ZSH/oh-my-zsh.sh
 
 if [ -f $HOME/.dotfiles/zsh/spaceship.zsh ]; then source $HOME/.dotfiles/zsh/spaceship.zsh; fi
 
+if [ -f $HOME/.zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh ]; then source $HOME/.zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh; fi
+
 if [ -f ~/.fzf.zsh  ]; then source ~/.fzf.zsh; fi
 
