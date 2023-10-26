e7cc08286aba97e4a54d81423e139d42373fc9d6
Kevin Murani <kevin.murani@booking.com>
2023-10-26 18:33:34 +0200

    Updates config to match v3 expectations for setup

--- vim/lua/plugins/indent_blankline.lua
+++ vim/lua/plugins/indent_blankline.lua
@@ -1,7 +1,4 @@
 vim.opt.list = true
 -- vim.opt.listchars:append "space:"
 
-require("indent_blankline").setup {
-    show_current_context = true,
-    show_current_context_start = true,
-}
+require("ibl").setup()
