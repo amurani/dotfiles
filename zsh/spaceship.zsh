659479836dcff9ff740f7df8d920769662a7f190
Kevin Murani <murani.kevin@gmail.com>
2023-10-29 12:38:38 +0100

    Removes  battery status on zsh spaceship

--- zsh/spaceship.zsh
+++ zsh/spaceship.zsh
@@ -2,7 +2,7 @@ source "$HOME/.oh-my-zsh/custom/themes/spaceship.zsh-theme"
 
 ZSH_THEME="spaceship"
 
-SPACESHIP_BATTERY_SHOW=true
+SPACESHIP_BATTERY_SHOW=false
 SPACESHIP_TIME_SHOW=true
 SPACESHIP_TIME_12HR=true
 SPACESHIP_GIT_STATUS_SHOW=false
