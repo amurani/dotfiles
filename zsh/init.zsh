bd172a2cbd355b8e0fb34dc3207532a6492c9b85
Kevin Murani <kevin.murani@booking.com>
2024-02-27 09:23:32 +0100

    Disables fish shell in favour of zsh

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -9,12 +9,13 @@ export ZSH="$HOME/.oh-my-zsh" # Path to your oh-my-zsh installation.
 source $ZSH/oh-my-zsh.sh
 
 # spaceship prompt for zsh
-# if [ -f $HOME/.dotfiles/zsh/spaceship.zsh ]; then source $HOME/.dotfiles/zsh/spaceship.zsh; fi
+if [ -f $HOME/.dotfiles/zsh/spaceship.zsh ]; then source $HOME/.dotfiles/zsh/spaceship.zsh; fi
 
 # starship prompt
-if [ -f $HOME/.dotfiles/zsh/starship.zsh ]; then source $HOME/.dotfiles/zsh/starship.zsh; fi
+# if [ -f $HOME/.dotfiles/zsh/starship.zsh ]; then source $HOME/.dotfiles/zsh/starship.zsh; fi
 
 # syntax highlighting for zsh
+# if [ -f source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]; then source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh; fi
 if [ -f $HOME/.zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh ]; then source $HOME/.zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh; fi
 
 # fzf
