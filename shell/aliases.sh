dbd55cfa30e589e3b815f56672a3b989cf7c2c8b
Kevin Murani <kevin.murani@booking.com>
2024-03-28 14:05:31 +0100

    Adds some long await git aliases to make life easier

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -4,6 +4,8 @@ alias mygit='git log --author="Kevin Murani"'
 alias gti='git'
 alias gut='git'
 alias gs='git fetch && git status'
+alias gaf='git add $(git status -s | awk "{ print \$2 }" | fzf)' # [g]it [a]dd via [f]zf
+alias gbf='git checkout $(git --no-pager branch | sed "s/\*/ /" | fzf)' # [g]it checkout [b]ranch via [f]zf
 
 # --------------- /git ------------------
 
