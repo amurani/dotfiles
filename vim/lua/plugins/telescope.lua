26ac254a2f3dd053bdfd4ff312ddfe5fb3d05ed1
Kevin Murani <kevin.murani@booking.com>
2024-03-28 11:19:03 +0100

    Adds a command for telescope buffer selection

--- vim/lua/plugins/telescope.lua
+++ vim/lua/plugins/telescope.lua
@@ -12,3 +12,4 @@ require("telescope").load_extension("recent_files")
 
 -- custom keymaps
 vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+vim.keymap.set('n', '<leader>bf', require('telescope.builtin').buffers, { desc = 'Show [B]u[f]fers' })
