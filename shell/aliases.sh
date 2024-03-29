90c70d3dba96c766f3d98052339c7d4734a92f29
Kevin Murani <murani.kevin@gmail.com>
2024-03-29 14:24:35 +0100

    Enable multifile selectoin via git add via fzf

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -5,7 +5,7 @@ alias gti='git'
 alias gut='git'
 alias gs='git fetch && git status'
 alias gaf='git add $(git status -s | awk "{ print \$2 }" | fzf)' # [g]it [a]dd via [f]zf
-alias gbf='git checkout $(git --no-pager branch | sed "s/\*/ /" | fzf)' # [g]it checkout [b]ranch via [f]zf
+alias gbf='git checkout $(git --no-pager branch | sed "s/\*/ /" | fzf --multi)' # [g]it checkout [b]ranch via [f]zf
 
 # --------------- /git ------------------
 
