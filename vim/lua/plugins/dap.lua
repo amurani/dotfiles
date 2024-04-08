c1ca163cb1c1583ef3939724f43397df401030fd
Kevin Murani <kevin.murani@booking.com>
2024-04-08 22:14:26 +0200

    Fixes the attach and launch dap configs to working state

--- vim/lua/plugins/dap.lua
+++ vim/lua/plugins/dap.lua
@@ -1,7 +1,25 @@
 local dap = require('dap')
 
+dap.set_log_level('ERROR')
+
+-- see: https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#node-debug2
+dap.adapters.node2 = {
+    type = 'executable',
+    command = 'node',
+    args = { os.getenv('HOME') .. '/dev/microsoft/vscode-node-debug2/out/src/nodeDebug.js' },
+}
+
 for _, language in ipairs({ "typescript", "javascript" }) do
     dap.configurations[language] = {
+        -- attach to a node process that has been started with
+        -- `--inspect` for longrunning tasks or `--inspect-brk` for short tasks
+        -- npm script -> `node --inspect-brk ./node_modules/.bin/vite dev`
+        -- the pwa-node adapter disconnected as soon as it attached to the process so we gave up on it
+        {
+            name = "Attach debugger to existing `node --inspect` process",
+            type = 'node2',
+            request = 'attach',
+        },
         -- debugger for only for single node typescript file
         language == "typescript" and {
             type = "pwa-node",
@@ -10,6 +28,9 @@ for _, language in ipairs({ "typescript", "javascript" }) do
             program = "${file}",
             cwd = "${workspaceFolder}",
             runtimeExecutable = "ts-node",
+            runtimeArgs = {
+                -- add/remove as needed
+            }
         } or nil,
         -- debugger for only for single node javascript file
         language == "javascript" and {
@@ -21,27 +42,6 @@ for _, language in ipairs({ "typescript", "javascript" }) do
             sourceMaps = true,
             runtimeExecutable = "node", -- redundant, but I want to be explicit
         } or nil,
-        -- attach to a node process that has been started with
-        -- `--inspect` for longrunning tasks or `--inspect-brk` for short tasks
-        -- npm script -> `node --inspect-brk ./node_modules/.bin/vite dev`
-        {
-            -- use nvim-dap-vscode-js's pwa-node debug adapter
-            type = "pwa-node",
-            -- attach to an already running node process with --inspect flag
-            -- default port: 9222
-            request = "attach",
-            -- allows us to pick the process using a picker
-            processId = require 'dap.utils'.pick_process,
-            -- name of the debug action you have to select for this config
-            name = "Attach debugger to existing `node --inspect` process",
-            -- for compiled languages like TypeScript or Svelte.js
-            sourceMaps = true,
-            resolveSourceMapLocations = {
-                "${workspaceFolder}/**",
-                "!**/node_modules/**" },
-            cwd = "${workspaceFolder}",
-            skipFiles = { "${workspaceFolder}/node_modules/**/*.js" },
-        },
         -- TODO: figure this one out
         {
             type = "pwa-node",
@@ -73,18 +73,21 @@ vim.fn.sign_define('DapBreakpoint',
 -- adding extra deps setups here as I don't need another config for this
 require("dap-vscode-js").setup({
     -- node_path = "node",                                                                          -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-    debugger_path = "/Users/kevinmurani/.vim/plugged/vscode-js-debug", -- Path to vscode-js-debug installation.
+    debugger_path = os.getenv('HOME') .. "/vscode-js-debug", -- Path to vscode-js-debug installation.
     -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
     adapters = {
-        'pwa-node',
+        'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost'
     },
-    log_file_path = "/tmp/dap_vscode_js.log" -- Path for file logging
-    -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
-    -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+    log_file_path = "/tmp/dap_vscode_js.log", -- Path for file logging
+    log_file_level = vim.log.levels.ERROR,    -- Logging level for output to file. Set to false to disable file logging.
+    log_console_level = vim.log.levels.ERROR, -- Logging level for output to console. Set to false to disable console output.
 })
 require("dapui").setup()
 require("nvim-dap-virtual-text").setup()
+vim.g.dap_virtual_text = true
 
 -- key bindings
 vim.api.nvim_set_keymap("n", "<leader>db", ":DapToggleBreakpoint<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>dr", ":DapToggleRepl<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>dc", ":DapContinue<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>dt", ":lua require('dapui').toggle({reset = true})<CR>", { noremap = true })
