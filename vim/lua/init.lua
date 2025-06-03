dea927d426f31b402ec68d4c0b063de998201dba
Kevin Murani <kevin.murani@booking.com>
2025-06-03 22:19:30 +0200

    chore: lua lsp formatted these files on it's own

--- vim/lua/init.lua
+++ vim/lua/init.lua
@@ -1,42 +1,42 @@
 -- Plugin Config Imports
 
 -- Improve NVIM UI
-require('plugins.lualine')
-require('plugins.bufferline')
-require('plugins.netrw')
+require("plugins.lualine")
+require("plugins.bufferline")
+require("plugins.netrw")
 
 -- IDEfy NVIM
-require('plugins.lspconfig')
-require('plugins.lspsaga')
-require('plugins.cmp')
-require('plugins.treesitter')
-require('plugins.telescope')
-require('plugins.mason')
-require('plugins.luasnip')
-require('plugins.sonarlint')
-require('plugins.conform')
+require("plugins.lspconfig")
+require("plugins.lspsaga")
+require("plugins.cmp")
+require("plugins.treesitter")
+require("plugins.telescope")
+require("plugins.mason")
+require("plugins.luasnip")
+require("plugins.sonarlint")
+require("plugins.conform")
 
 -- General QoL tools for coding
-require('plugins.nvim-autopairs')
-require('plugins.indent_blankline')
+require("plugins.nvim-autopairs")
+require("plugins.indent_blankline")
 
 -- Language specific QoL tools
-require('plugins.dap')
+require("plugins.dap")
 
 -- /Plugin Config Imports
 
 -- Plugin No Config Imports
-require('gitsigns').setup()
-require('diagflow').setup({
-    border_chars = {
-        top_left = "┌",
-        top_right = "┐",
-        bottom_left = "└",
-        bottom_right = "┘",
-        horizontal = "─",
-        vertical = "│"
-    },
-    show_borders = true,
+require("gitsigns").setup()
+require("diagflow").setup({
+	border_chars = {
+		top_left = "┌",
+		top_right = "┐",
+		bottom_left = "└",
+		bottom_right = "┘",
+		horizontal = "─",
+		vertical = "│",
+	},
+	show_borders = true,
 })
 -- /Plugin No Config Imports
 
@@ -48,15 +48,15 @@ require('diagflow').setup({
 -- require('plugins.sourcegraph')
 
 require("codesnap").setup({
-    mac_window_bar = false,
-    has_breadcrumbs = false,
-    bg_color = "#edeff3ff",
-    watermark = "",
-    code_font_family = "Monaco Nerd Font",
+	mac_window_bar = false,
+	has_breadcrumbs = false,
+	bg_color = "#edeff3ff",
+	watermark = "",
+	code_font_family = "Monaco Nerd Font",
 })
 
-require('lint').linters_by_ft = {
-    kotlin = { 'ktlint' },
+require("lint").linters_by_ft = {
+	kotlin = { "ktlint" },
 }
 
 require("coverage").setup()
